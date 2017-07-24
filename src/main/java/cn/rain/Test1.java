f3d3c0652ee1b26bea52ead3a4a9d12bf1b564be
victorrrru <942021266@qq.com>
2017-07-24 17:12:37 +0800

    Signed-off-by: victorrrru <942021266@qq.com>

---
+++ src/main/java/cn/rain/Test1.java
@@ -0,0 +1,18 @@
+package cn.rain;
+
+public class Test1 {
+
+    private String name;
+
+    public Test1(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/cn/rain/Test2.java
@@ -0,0 +1,14 @@
+package cn.rain;
+
+public class Test2 {
+
+    private String sex;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
